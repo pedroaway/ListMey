1c75be6b4406444dad2fb4d44999bcd7eea7abbb
pedroaway <ppgabriell00@gmail.com>
2023-01-28 18:47:19 -0300

    Atualzações no front

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-prompt("teste")
+
+
+var task_list = document.createElement('dl');
+var task = document.createElement('dt');
+var conteudo = document.createTextNode('tarefa');
+task.appendChild(conteudo);
+task_list.appendChild(task);
+
+var card = document.getElementsByClassName("left-card");
+var card_tarefas = getelementbytagname('ul');
+
